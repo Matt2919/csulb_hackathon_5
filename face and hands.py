7e00388c67500cf86ec2dc658d30b525ef9244ec
m2919 <m.9mazzone@gmail.com>
2025-10-25 11:17:37 -0700

    six seven

--- face and hands.py
+++ face and hands.py
@@ -5,7 +5,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 mp_face_detection = mp.solutions.face_detection
 
-
+prev = -1
 cap = cv2.VideoCapture(0)
 with mp_face_detection.FaceDetection(
         model_selection=0, min_detection_confidence=0.5) as face_detection, \
@@ -30,6 +30,28 @@ with mp_face_detection.FaceDetection(
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks and results.multi_handedness:
+        for hand_landmarks, handedness in zip(
+                results.multi_hand_landmarks, results.multi_handedness):
+
+            # Check for right hand
+
+            if handedness.classification[0].label == "Left":
+                #print("Right hand landmarks:")
+                lm = hand_landmarks.landmark[9]
+                if prev ==-1:
+                    prev = lm.y
+                if prev != -1:
+                   # print ("prev",prev,"new", lm.y)
+                    if abs(lm.y - prev) > 0.05:
+                        print("SIX SEVEN")
+                        #exit()
+                    else:
+                        print("no")
+                    prev = lm.y
+
+                #print(f"Landmark: x={lm.x:.3f}, y={lm.y:.3f}, z={lm.z:.3f}")
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -38,6 +60,7 @@ with mp_face_detection.FaceDetection(
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
+
     if results2.detections:
         for detection in results2.detections:
             mp_drawing.draw_detection(image, detection)
